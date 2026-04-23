1790208c1367cd41195607de92428a39e1c24f14
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-04-23 11:12:30 +0500

    Implement user signup functionality with validation and hashing

---
+++ src/app/auth/routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import type { Router } from "express";
+import AuthenticationControlller from "./controlller";
+
+const authenticationController = new AuthenticationControlller()
+
+export const authRouter:Router = express.Router();
+
+
+authRouter.post('/sign-up',authenticationController.handleSignup.bind(authenticationController));
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,22 +1,20 @@
 import express from 'express'
 import type { Express } from 'express'
+import { authRouter } from './auth/routes';
 
 
 export function createApplication(): Express {
     const app = express()
 
     // Middlewares
-
+    app.use(express.json())
 
     // Routes
     app.get('/', (req:any, res:any) => {
         return res.json({ message: 'Welcome to AR Auth Service, It`s Working' })
     });
 
-    app.post('/data', (req:any, res:any) => {
-        return res.json({ message: 'Welcome to AR Auth Service , Data route is working' })
-    })
-
+    app.use('/auth',authRouter)
 
     return app
 }
